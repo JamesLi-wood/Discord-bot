ca4936e9e02295bbe59684a2ac8b44bfd80fbccd
james <jamesliwood@gmail.com>
2023-10-05 04:29:29 -0400

    added client login and messageCreate function

---
+++ index.js
@@ -0,0 +1,90 @@
+// Require the necessary discord.js classes
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+
+// Create a new client instance
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  /* Prevents an infinite loop if message content
+  is a certain phrase. */
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content == "hello") {
+    message.reply("hello");
+  }
+
+  console.log(message.content);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.TOKEN);
+
+// client.commands = new Collection();
+
+// const foldersPath = path.join(__dirname, "commands");
+// const commandFolders = fs.readdirSync(foldersPath);
+
+// for (const folder of commandFolders) {
+//   const commandsPath = path.join(foldersPath, folder);
+//   const commandFiles = fs
+//     .readdirSync(commandsPath)
+//     .filter((file) => file.endsWith(".js"));
+//   for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//     const command = require(filePath);
+//     // Set a new item in the Collection with the key as the command name and the value as the exported module
+//     if ("data" in command && "execute" in command) {
+//       client.commands.set(command.data.name, command);
+//     } else {
+//       console.log(
+//         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+//       );
+//     }
+//   }
+// }
+
+// client.on(Events.InteractionCreate, async (interaction) => {
+//   if (!interaction.isChatInputCommand()) return;
+
+//   const command = interaction.client.commands.get(interaction.commandName);
+
+//   if (!command) {
+//     console.error(`No command matching ${interaction.commandName} was found.`);
+//     return;
+//   }
+
+//   try {
+//     await command.execute(interaction);
+//   } catch (error) {
+//     console.error(error);
+//     if (interaction.replied || interaction.deferred) {
+//       await interaction.followUp({
+//         content: "There was an error while executing this command!",
+//         ephemeral: true,
+//       });
+//     } else {
+//       await interaction.reply({
+//         content: "There was an error while executing this command!",
+//         ephemeral: true,
+//       });
+//     }
+//   }
+// });
