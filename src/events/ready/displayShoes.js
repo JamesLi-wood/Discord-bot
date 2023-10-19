fa5b1287c3699469f3ea47bda68febc6931debb2
james <jamesliwood@gmail.com>
2023-10-19 03:27:16 -0400

    Will make a post when the api updates (new show just got added)

--- src/events/ready/displayShoes.js
+++ src/events/ready/displayShoes.js
@@ -2,22 +2,27 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports = async (client) => {
   // Gets the channel we want the bot to message at.
-  const channel = await client.channels.cache.get("1159388897747410987");
+  const channel = await client.channels.cache.get("1162247722460516382");
   // In case channel doesn't exist.
   if (!channel) return;
+  
+  let prevShoe = null;
 
   setInterval(() => {
     fetch(process.env.NIKE_API)
       .then((res) => res.json())
       .then((data) => {
-        const shoes = data.data.products.products;
+        const currShoe = data.data.products.products;
 
+        if (prevShoe == currShoe[0].title) return;
+
+        prevShoe = currShoe[0].title;
         const embed = new EmbedBuilder()
-          .setImage(shoes[0].images.portraitURL)
-          .setTitle(shoes[0].title)
+          .setImage(currShoe[0].images.portraitURL)
+          .setTitle(currShoe[0].title)
           .addFields({
             name: "Price",
-            value: `${shoes[0].price.currentPrice}`,
+            value: `${currShoe[0].price.currentPrice}`,
           })
           .setColor("Green");
 
